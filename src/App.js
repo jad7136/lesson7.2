04310745eb607fbda02a3688bdb41baeb0829b3c
jad7136 <jad7136@psu.edu>
2024-04-24 19:29:18 -0400

    hi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Movie from "./movie.js"
+import React from 'react';
+import './movie.css';
+//This uses 3 movies components in App
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"> 
+      <h1>List of Movies</h1>
+      <div className= "movies row">
+        <div className='col-lg-4  col-md-6 col-sm-12'>
+        <Movie/>
+        </div>
+        </div>
+        <div className='col-lg-4  col-md-6 col-sm-12'>
+        <Movie/>
+      </div>
+      <div className='col-lg-4  col-md-6 col-sm-12'>
+          <Movie/>
+        </div>
+      </div>
   );
 }
-
 export default App;
